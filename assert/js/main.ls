c991232c9679ecf1e5cf150b7f191f575326a883
damonshen <thanatos1710@hotmail.com>
2014-07-25 18:44:00 +0800

    modify the gulp plugins and use js for adding style

--- assert/js/main.ls
+++ assert/js/main.ls
@@ -1,1 +1,8 @@
 console.log \hello
+
+$ ->
+  $ '#code div' .each (index)->
+    $ this .html "#index test"
+    $ this .css \background, \blue
+
+
--- show.py
+++ show.py
@@ -2,10 +2,12 @@ from pyh import *
 
 page = PyH('Code visualization')
 page.addCSS('./assert/css/main.css')
+page.addJS('./bower_components/jquery/dist/jquery.min.js')
 page.addJS('./assert/js/main.js')
 page << div('title', id='title')
 code = page << div(id='code')
 code << 'test' 
+code.attributes['correlation'] = 50
 code << div('green', cl='green')
 code << div('green-red', cl='green-red')
 code << div('red', cl='red')
